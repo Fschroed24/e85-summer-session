e408c6e13b159d0d5a49db585b7367c4dd289c1c
Franz Schroder <fschroeder2411@gmail.com>
2026-06-26 00:00:19 -0700

    my decoder + testbench, test vectors and the 9 outputs 0 errors.

--- e85/Lab2/lab2_XX/aludecoder.sv
+++ e85/Lab2/lab2_XX/aludecoder.sv
@@ -25,6 +25,35 @@ module aludecoder(input  logic [1:0] ALUOp,
                   output logic [2:0] ALUControl);
 
    // ---- STUB: replace with your implementation ----
-   assign ALUControl = 3'b000;
+   // assign ALUControl = 3'b000;
+
+
+   // this is declaring the 4 helper signals that will be used in the modules, defined in always_comb block? 
+   logic T1;
+   logic R; 
+   logic Z; 
+   logic S; 
+   // i dont know conventional used variables so i picked random ones 
+
+   always_comb begin
+      // defined helper signals based on my schematic and boolean equations, these are used to make the ALUControl bits
+      T1 = (~ALUOp[1] & ALUOp[0]);
+      R = (ALUOp[1] & ~ALUOp[0]);
+      Z = ~(funct3[2] | funct3[1] | funct3[0]);
+      S = (funct3[1] & ~funct3[0]); 
+
+      // now that i have made these helper signals, i wanna make my outputs for ALUControl[2:0] 
+      ALUControl[0] = (T1) | 
+      (R & Z & (op5 & funct75)) | 
+      (R & S);
+      // the above definition defines the LSB for our output ALUControl based on my boolean equations and optimizations in my schematic 
+      ALUControl[1] = (R & funct3[2] & funct3[1]); 
+      // this one is for ALUControl[1] and apparently Systemverilog decides if it makes a 3 input AND gate or 2 different 2 input AND gates
+      ALUControl[2] = (R & S & ~funct3[2]); 
+      // this is for the MSB of ALUControl, same idea as above... 
+      
+
+
+   end
 
 endmodule
